9557cf223403d2380db7cbb80d9855fe81e305ec
Jackeline Castro <jackelinecastrocardoso@hotmail.com>
2023-06-27 17:43:40 -0300

    novo nome do repositorio

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Tela-de-login-react-e-vite",
+  base: "/tela-login-react-vite",
 });
